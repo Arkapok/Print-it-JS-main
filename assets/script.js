093a3cf08ccaf8f6a7d9782461f13405a5c8dc06
Erwan Charef <erwan.charef@gmail.com>
2025-06-29 01:44:50 +0200

    Add bullets points

--- assets/script.js
+++ assets/script.js
@@ -24,6 +24,43 @@ const slides = [
 	const leftArrow = document.querySelector('.arrow_left');
 	const rightArrow = document.querySelector('.arrow_right');
 	const dotsContainer = document.querySelector('.dots');
-
 	//Valeur initiale de 0.
-	let Index = 0;
+	let currentSlide = 0;
+
+	// Fonction pour générer les points dynamiquement
+    function createDots(numberOfDots) {
+      for (let i = 0; i < numberOfDots; i++) {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
+        dotsContainer.appendChild(dot);
+      }
+
+      // Sélection premier point et ajoutez-y la classe "dot_selected"
+      const dots = document.querySelectorAll('.dot');
+      dots[currentSlide].classList.add('dot_selected');
+    }
+
+    //logique pour changer le point sélectionné
+    function updateCarousel() {
+      const selectedDot = document.querySelector('.dot_selected');
+      if (selectedDot) {
+        selectedDot.classList.remove('dot_selected');
+      }
+
+      //récupère tous les éléments HTML des points (dots).
+      const dots = document.querySelectorAll('.dot');
+      dots[currentSlide].classList.add('dot_selected');
+      showSlide(currentSlide);
+    }
+
+    //affiche le contenu en fonction de l'index de diapositive actuel
+    function showSlide(slideIndex) {
+      if (slideIndex >= 0 && slideIndex < slides.length) {
+        bannerImage.src = './assets/images/slideshow/' + slides[slideIndex].image;
+        bannerTagline.innerHTML = slides[slideIndex].tagLine;
+      }
+    }
+
+    //Génére les points dynamiquement
+    const numberOfSlides = slides.length;
+    createDots(numberOfSlides);
